5db1b8ec81500362e6a3510786c22a41eabf53ad
frans.mazhar <frans.mazhar@gmail.com>
2022-10-20 22:25:24 +0700

    first

---
+++ src/pages/customer-list/CustomerList.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CContainer,
+  CCardHeader,
+  CListGroup,
+  CListGroupItem,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalTitle,
+  CModalHeader,
+  CCol,
+  CRow,
+  // DocsLink,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilUser } from '@coreui/icons'
+
+const CustomerList = () => {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [customers, setCustomers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder1.typicode.com/posts?_limit=10')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setCustomers(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [])
+
+  function handleClick(e) {
+    e.preventDefault()
+    console.log('click')
+  }
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-top">
+      <CModal
+        alignment="center"
+        backdrop="static"
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      >
+        <CModalHeader>
+          <CModalTitle>New Customer Confirmation</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <h5>Registration Information</h5>
+          <CContainer>
+            <CRow className="align-items-start">
+              <CCol>Instansi</CCol>
+              <CCol>One of three columns</CCol>
+            </CRow>
+            <CRow className="align-items-start">
+              <CCol>Address</CCol>
+              <CCol>One of three columns</CCol>
+            </CRow>
+            <CRow className="align-items-start">
+              <CCol>Email</CCol>
+              <CCol>One of three columns</CCol>
+            </CRow>
+          </CContainer>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="secondary" onClick={() => setModalVisible(false)}>
+            Cancel
+          </CButton>
+          <CButton color="primary">New Customer</CButton>
+        </CModalFooter>
+      </CModal>
+      <CContainer>
+        <CCard className="mb-4">
+          <CCardHeader>Customer List</CCardHeader>
+          <CCardBody>
+            <CListGroup>
+              <CListGroupItem component="button" onClick={() => setModalVisible(true)}>
+                gdfh justo s
+              </CListGroupItem>
+              <CListGroupItem component="button">Dapibus ac facilisis in</CListGroupItem>
+              <CListGroupItem component="button">Morbi leo risus</CListGroupItem>
+              <CListGroupItem component="button">Porta ac consectetur ac</CListGroupItem>
+              <CListGroupItem component="button" disabled>
+                Vestibulum at eros
+              </CListGroupItem>
+            </CListGroup>
+          </CCardBody>
+        </CCard>
+      </CContainer>
+    </div>
+  )
+}
+
+export default CustomerList
---
+++ src/pages/registration/Registration.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilUser } from '@coreui/icons'
+
+const Registration = () => {
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={9} lg={7} xl={6}>
+            <CCard className="mx-4">
+              <CCardBody className="p-4">
+                <CForm>
+                  <h1>Register</h1>
+                  <p className="text-medium-emphasis">Register your company</p>
+
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Company name" autoComplete="username" />
+                  </CInputGroup>
+
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Company Address" />
+                  </CInputGroup>
+
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Business Registration ID / SIUPP" />
+                  </CInputGroup>
+
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Tax ID / NPWP" />
+                  </CInputGroup>
+
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>@</CInputGroupText>
+                    <CFormInput placeholder="Person Name" />
+                  </CInputGroup>
+
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>@</CInputGroupText>
+                    <CFormInput placeholder="Email" autoComplete="email" />
+                  </CInputGroup>
+
+                  <div className="d-grid">
+                    <CButton color="success">Submit</CButton>
+                  </div>
+
+                  <p className="text-medium-emphasis">
+                    Check your email for registration information
+                  </p>
+                </CForm>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Registration
