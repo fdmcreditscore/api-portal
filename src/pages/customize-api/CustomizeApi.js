e3b7bd878c776ad50bd822551c35f0a7c6e681a0
frans.mazhar <frans.mazhar@gmail.com>
2022-10-30 00:13:01 +0700

    update

--- src/pages/customize-api/CustomizeApi.js
+++ src/pages/customize-api/CustomizeApi.js
@@ -18,16 +18,46 @@ import {
   CFormCheck,
   CCol,
   CRow,
+  CForm,
   // DocsLink,
 } from '@coreui/react'
 
 const CustomizeApi = () => {
+  const cloneDeep = require('lodash.clonedeep')
   const [modalVisible, setModalVisible] = useState(false)
   const [modules, setModules] = useState([])
-  const [isChecked, setIsChecked] = useState(new Array(toppings.length).fill(false))
+  const [apisChooseState, setApisChooseState] = useState(cloneDeep(apiList))
 
-  const handleOnChange = () => {
-    setIsChecked(!isChecked)
+  const handleOnChange = (e) => {
+    let apis = apisChooseState.map((element) => {
+      element.include = element.name === e.target.id ? !element.include : element.include
+      return element
+    })
+    setApisChooseState(apis)
+  }
+
+  function checkDefaultCheck(e) {
+    console.log(e)
+    return apisChooseState.find((api) => api.name === e).include === true
+  }
+
+  const handleOnSubmit = () => {
+    const newapis = apisChooseState.filter((el) => el.include === true).map((a) => a.name)
+    const bodymsg = { modulNames: newapis }
+    console.log(newapis)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(bodymsg),
+    }
+    fetch('http://localhost:8900/mgmt/v1/customers/AQ82290/modulset', requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
   }
 
   useEffect(() => {
@@ -48,38 +78,82 @@ const CustomizeApi = () => {
         alignment="center"
         backdrop="static"
         visible={modalVisible}
+        onShow={() => console.log(apisChooseState)}
         onClose={() => setModalVisible(false)}
       >
-        <CModalHeader>
-          <CModalTitle>Add New API Set</CModalTitle>
-        </CModalHeader>
-        <CModalBody>
-          <h5>Choose API </h5>
-          <CContainer>
-            <CRow className="align-items-start">
-              <CCol>
-                <CFormCheck id="telco:active" label="telco:active" />
-                <CFormCheck id="telco:jeniskartu" label="telco:jeniskartu" />
-                <CFormCheck id="telco:billing" label="telco:billing" />
-                <CFormCheck id="telco:paketdata" label="telco:paketdata" />
-                <CFormCheck id="telco:saldo" label="telco:saldo" />
-                <CFormCheck id="telco:tenure" label="telco:tenure" />
-                <CFormCheck id="telco:expire" label="telco:expire" />
-                <CFormCheck id="telco:dataexpire" label="telco:dataexpire" />
-                <CFormCheck id="telco:aktivasi" label="telco:aktivasi" />
-              </CCol>
-              <CCol>
-                <CFormCheck id="identity:matchface" label="identity:matchface" />
-              </CCol>
-            </CRow>
-          </CContainer>
-        </CModalBody>
-        <CModalFooter>
-          <CButton color="secondary" onClick={() => setModalVisible(false)}>
-            Cancel
-          </CButton>
-          <CButton color="primary">Register</CButton>
-        </CModalFooter>
+        <CForm onSubmit={handleOnSubmit}>
+          <CModalHeader>
+            <CModalTitle>Add New API Set</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <h5>Choose API </h5>
+            <CContainer>
+              <CRow className="align-items-start">
+                <CCol onChange={handleOnChange}>
+                  <CFormCheck
+                    id="telco:active"
+                    label="telco:active"
+                    // defaultChecked="true"
+                    defaultChecked={
+                      apisChooseState.find((e) => e.name === 'telco:active').include === true
+                    }
+                  />
+                  <CFormCheck
+                    id="telco:jeniskartu"
+                    label="telco:jeniskartu"
+                    defaultChecked={checkDefaultCheck('telco:jeniskartu')}
+                  />
+                  <CFormCheck
+                    id="telco:billing"
+                    label="telco:billing"
+                    defaultChecked={checkDefaultCheck('telco:billing')}
+                  />
+                  <CFormCheck
+                    id="telco:paketdata"
+                    label="telco:paketdata"
+                    defaultChecked={checkDefaultCheck('telco:paketdata')}
+                  />
+                  <CFormCheck
+                    id="telco:saldo"
+                    label="telco:saldo"
+                    defaultChecked={checkDefaultCheck('telco:saldo')}
+                  />
+                  <CFormCheck
+                    id="telco:tenure"
+                    label="telco:tenure"
+                    defaultChecked={checkDefaultCheck('telco:tenure')}
+                  />
+                  <CFormCheck id="telco:expire" label="telco:expire" />
+                  <CFormCheck
+                    id="telco:dataexpire"
+                    label="telco:dataexpire"
+                    defaultChecked={checkDefaultCheck('telco:dataexpire')}
+                  />
+                  <CFormCheck
+                    id="telco:aktivasi"
+                    label="telco:aktivasi"
+                    defaultChecked={checkDefaultCheck('telco:aktivasi')}
+                  />
+                </CCol>
+                <CCol onChange={handleOnChange}>
+                  <CFormCheck
+                    id="identity:matchface"
+                    label="identity:matchface"
+                    defaultChecked={checkDefaultCheck('identity:matchface')}
+                  />
+                </CCol>
+              </CRow>
+            </CContainer>
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="secondary" onClick={() => setModalVisible(false)}>
+              Cancel
+            </CButton>
+            <CButton color="primary" type="submit">
+              Register
+            </CButton>
+          </CModalFooter>
+        </CForm>
       </CModal>
       <CContainer>
         <CCard className="mb-4">
@@ -88,14 +162,33 @@ const CustomizeApi = () => {
             <CListGroup>
               {modules.map((modul) => {
                 return (
-                  <CListGroupItem component="button" key={modul.modulsetCode}>
+                  <CListGroupItem
+                    component="button"
+                    key={modul.modulsetCode}
+                    onClick={() => {
+                      let registeredApi = modul.moduls.map((n) => n.modulName)
+                      let clonedApiList = cloneDeep(apiList)
+                      let newApiList = clonedApiList.map((n) => {
+                        n.include = registeredApi.includes(n.name) ? true : false
+                        return n
+                      })
+                      setApisChooseState(newApiList)
+                      setModalVisible(true)
+                    }}
+                  >
                     {modul.endpoint}
                   </CListGroupItem>
                 )
               })}
             </CListGroup>
             <CCardFooter>
-              <CButton color="info" onClick={() => setModalVisible(true)}>
+              <CButton
+                color="info"
+                onClick={() => {
+                  setApisChooseState(cloneDeep(apiList))
+                  setModalVisible(true)
+                }}
+              >
                 Add New Custom API Set
               </CButton>
             </CCardFooter>
--- src/pages/customize-api/api-list.js
+++ src/pages/customize-api/api-list.js
@@ -1,45 +1,57 @@
-export const apiList = [{
-        "name": "telco:jeniskartu",
-        "price": 4000
-    },
-    {
-        "name": "telco:billing",
-        "price": 3500
-    },
-    {
-        "name": "telco:paketdata",
-        "price": 3500
-    },
-    {
-        "name": "telco:saldo",
-        "price": 5000
-    },
-    {
-        "name": "telco:quotadata",
-        "price": 3500
-    },
-    {
-        "name": "telco:tenure",
-        "price": 3500
-    },
-    {
-        "name": "telco:expire",
-        "price": 4000
-    },
-    {
-        "name": "telco:dataexpire",
-        "price": 3000
-    },
-    {
-        "name": "telco:aktivasi",
-        "price": 2000
-    },
-    {
-        "name": "telco:active",
-        "price": 3500
-    },
-    {
-        "name": "identity:matchface",
-        "price": 1000
-    },
+export const apiList = [
+  {
+    name: 'telco:jeniskartu',
+    price: 4000,
+    include: false,
+  },
+  {
+    name: 'telco:billing',
+    price: 3500,
+    include: false,
+  },
+  {
+    name: 'telco:paketdata',
+    price: 3500,
+    include: false,
+  },
+  {
+    name: 'telco:saldo',
+    price: 5000,
+    include: false,
+  },
+  {
+    name: 'telco:quotadata',
+    price: 3500,
+    include: false,
+  },
+  {
+    name: 'telco:tenure',
+    price: 3500,
+    include: false,
+  },
+  {
+    name: 'telco:expire',
+    price: 4000,
+    include: false,
+  },
+  {
+    name: 'telco:dataexpire',
+    price: 3000,
+    include: false,
+  },
+  {
+    name: 'telco:aktivasi',
+    price: 2000,
+    include: false,
+  },
+  {
+    name: 'telco:active',
+    price: 3500,
+    include: false,
+  },
+  {
+    name: 'identity:matchface',
+    price: 1000,
+    include: false,
+  },
 ]
